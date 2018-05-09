dedbe646c63aaf718eb93ef5c55ea2f4c0751b8a
Everett <everett.bowline@gmail.com>
2018-05-09 02:35:02 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -22,3 +22,19 @@ class MP3Importer
 >>>>>>> 725816b46ea0d5cfe03b71696959c159a2e54b27
   end
 end
+
+class MP3Importer 
+  attr_accessor :path 
+  def initialize (music_path)
+    @path = music_path
+  end
+  def files
+    Dir.chdir(@path)
+    Dir.glob("*.mp3")
+    files = []
+    Dir.foreach(".") do |file|
+      files << file
+      if file.include?(".mp3")
+        end
+        files
+  end
